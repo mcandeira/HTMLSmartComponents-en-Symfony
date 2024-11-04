1c331145f30fa1516ae05c760bb4a12e4682c292
Miguel Candeira Carrera <mcandeira.siweb@gmail.com>
2024-11-04 13:40:32 +0100

    Front v0.0.5 Cache Enabled

--- src/Controller/FrontController.php
+++ src/Controller/FrontController.php
@@ -4,8 +4,10 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Attribute\Cache;
 use Symfony\Component\Routing\Attribute\Route;
 
+#[Cache(public: true, maxage: 300, staleWhileRevalidate: 300)]
 class FrontController extends AbstractController
 {
     #[Route('/', name: 'app_home')]
@@ -14,6 +16,7 @@ class FrontController extends AbstractController
         return $this->render('base.html.twig');
     }
 
+    
     #[Route('/{element<jugador|coach|club>}/{action<crear|registrar|eliminar|listar>}', name: 'app_actions')]
     public function actions(string $element, string $action): Response
     {
